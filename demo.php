08cd0e42f75204d060b4853823fe541fe3882d23
unknown <rzulueta362@gmail.com>
2024-01-22 18:35:39 -0800

    New File

---
+++ demo.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Simple Form Processing</title>
+</head>
+
+<body>
+     <form action=""form_1.php" method="post">
+    firstname:
+    <input type="text" name="firstname" required/>
+    <br>
+    <br>
+    lastname:
+    <input type="text" name="lastname" required/>
+    <br>
+    <br>
+    address:
+    <input type="text" name="address" required/>
+    <br>
+    <br>
+    password:
+    <input type="text" name="password" required/>
+    <br>
+    <br>
+    <input type="submit" value=Submit>
+</form>
+</body>
+<html>
