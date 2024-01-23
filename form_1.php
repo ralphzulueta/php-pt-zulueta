3b40a49dc80c69debeb9e464c04218df45208b35
unknown <rzulueta362@gmail.com>
2024-01-22 19:16:37 -0800

    New File

---
+++ form_1.php
@@ -0,0 +1,11 @@
+<html>
+  <body>
+    
+Welcome <?php echo $_POST["fistname"] , $_POSt["fistname"]; ?><br>
+
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your address is: <?php echo $_POSt["address"]; ?>
+
+
+</body>
+</html>
